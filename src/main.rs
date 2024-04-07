f38b2e65c5e69ba82c3ff4d40e3d4d637826a52a
Zugeo <axelatlason2003@gmail.com>
2024-04-07 13:39:13 +0000

    added ability to convert and watch directories

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod source;
 mod token;
 mod emitter;
 
-use std::path::PathBuf;
+use std::{ffi::OsStr, path::{Path, PathBuf}};
 
 use clap::{Parser, Subcommand};
 use scanner::Scanner;
@@ -39,11 +39,64 @@ enum Commands {
     }
 }
 
-fn transpile_from_to(path: PathBuf, output: PathBuf) -> Result<()> {
-    let input = std::fs::read_to_string(path).context("Failed to read input file")?;
+fn check_extension(path: &Path, ext: &str) -> bool {
+    return path.extension().and_then(OsStr::to_str) == Some(ext);
+}
+
+fn transpile_file(path: &Path, output: &Path) -> Result<()> {
+    let input = std::fs::read_to_string(path)
+        .context("Failed to read input file")?;
     let res = transpile(&input)?;
+    let parent = output.parent()
+        .context(format!("Failed to get parent directory of file {output:?}"))?;
+    std::fs::create_dir_all(parent)?;
     std::fs::write(output, res).context("Failed to write transpiled code to file")?;
     Ok(())
+}
+
+fn transpile_dir(input: &Path, dir: &Path, output: &Path) -> Result<()> {
+    let cwd = std::env::current_dir()?;
+    let base_path: PathBuf = if input.is_absolute() { input.to_path_buf() } else { cwd.join(input) };
+
+    for dir_entry in dir.read_dir()? {
+        let path_buf = dir_entry?.path();
+        let path = Path::new(&path_buf);
+
+        if !path.is_dir() && !check_extension(path, "nhtml") {
+            continue;
+        }
+
+        if path.is_dir() {
+            transpile_dir(input, path, output)?;
+        } else {
+            let relative_out = if path.is_absolute() { path.strip_prefix(&base_path)? } else { path.strip_prefix(input)? };
+            let out = output.join(relative_out)
+                .with_extension("html");
+
+            transpile_file(
+                path,
+                &out
+            )?;
+        }
+    }
+
+    Ok(())
+}
+
+fn transpile_from_to(path: &Path, output: &Path) -> Result<()> {
+    if path.is_dir() {
+        return transpile_dir(path, path, output);
+    }
+
+    if output.is_dir() {
+        let file_name = path.file_name()
+            .context("Failed to get file name")?;
+        let out_file = output.join(file_name)
+            .with_extension("html");
+        return transpile_file(path, &out_file);
+    }
+
+    transpile_file(path, output)
 }
 
 fn main() -> anyhow::Result<()> {
@@ -51,29 +104,79 @@ fn main() -> anyhow::Result<()> {
 
     match cli.command {
         Commands::Convert { path, output } => {
-            transpile_from_to(path, output)?;
+            if path.is_dir() && output.is_file() {
+                eprintln!("Cannot output directory to a file");
+                return Ok(());
+            }
+            transpile_from_to(&path, &output)?;
         },
         Commands::Watch { path, output } => {
-            transpile_from_to(path.clone(), output.clone())?;
+            if path.is_dir() && output.is_file() {
+                eprintln!("Cannot output directory to a file");
+                return Ok(());
+            }
+            transpile_from_to(&path, &output)?;
             println!("Watching '{}'. Press CTRL-C to quit", path.to_str().unwrap());
-            watch(path, output)?;
+            watch(&path, &output)?;
         },
     }
 
     Ok(())
 }
 
-fn watch(input: PathBuf, output: PathBuf) -> Result<()> {
+fn watch(input: &Path, output: &Path) -> Result<()> {
     use notify::{Watcher, RecursiveMode, RecommendedWatcher, Config};
 
     let (tx, rx) = std::sync::mpsc::channel();
-    let mut watcher = RecommendedWatcher::new(tx, Config::default()).context("Failed to create watcher")?;
-    watcher.watch(input.as_ref(), RecursiveMode::Recursive).context("Failed to start watcher on path")?;
 
-    loop {
-        let _ = rx.recv().context("Watcher failed")?;
-        transpile_from_to(input.clone(), output.clone())?;
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())
+        .context("Failed to create watcher")?;
+
+    watcher.watch(&input, RecursiveMode::Recursive)
+        .context("Failed to start watcher on path")?;
+
+    for res in rx {
+        match res {
+            Ok(event) => watch_event(&event, input, output)?,
+            Err(err) => println!("err: {err:?}"),
+        }
     }
+
+    Ok(())
+}
+
+fn watch_event(event: &notify::Event, input: &Path, output: &Path) -> Result<()> {
+    if event.paths[0].is_dir() {
+        return Ok(());
+    }
+
+    if !matches!(event.kind, notify::EventKind::Modify(_)) {
+        return Ok(());
+    }
+
+    if !check_extension(&event.paths[0], "nhtml") {
+        return Ok(());
+    }
+
+    let cwd = std::env::current_dir()?;
+    let base_path: PathBuf = if input.is_absolute() { input.to_path_buf() } else { cwd.join(input) };
+
+    if output.is_file() {
+        transpile_file(
+            &event.paths[0],
+            &output
+        )?;
+    } else {
+        let relative_out = event.paths[0].strip_prefix(base_path)?;
+        let out = output.join(relative_out)
+            .with_extension("html");
+
+        transpile_file(
+            &event.paths[0],
+            &out
+        )?;
+    }
+    Ok(())
 }
 
 #[cfg(test)]
